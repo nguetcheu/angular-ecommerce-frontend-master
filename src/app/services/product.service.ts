947e2de288a40081b765bb2803829a9b5b74f2ec
nguetcheu <dnguetcheu@gmail.com>
2023-01-11 22:41:41 +0100

    mise a jour de product service pour l'usage du lien de pagination

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -16,11 +16,23 @@ export class ProductService {
   constructor(private httpClient: HttpClient) {}
 
   getProduct(theProductId: number): Observable<Product> {
-
     // need to build URL based on product id
     const productUrl = `${this.baseUrl}/${theProductId}`;
 
     return this.httpClient.get<Product>(productUrl);
+  }
+
+  getProductListPaginate(
+    thePage: number,
+    thePageSize: number,
+    theCategoryId: number
+  ): Observable<GetResponseProducts> {
+    // need to build URL based on category id, page and size
+    const searchUrl =
+      `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}` +
+      `&page=${thePage}&size=${thePageSize}`;
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
   }
 
   getProductList(theCategoryId: number): Observable<Product[]> {
@@ -53,6 +65,12 @@ export class ProductService {
 interface GetResponseProducts {
   _embedded: {
     products: Product[];
+  };
+  page: {
+    size: number;
+    totalElements: number;
+    totalPages: number;
+    number: number;
   };
 }
 
