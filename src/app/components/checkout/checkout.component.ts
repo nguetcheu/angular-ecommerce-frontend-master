aeb02b8b09fbf0906a7b39a84f92f648b6f45a69
nguetcheu <dnguetcheu@gmail.com>
2023-05-22 14:03:41 +0200

    optimisation de la séléction de année au niveau de la carte bancaire

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -90,4 +90,28 @@ export class CheckoutComponent implements OnInit {
       " L'adresse email est " + this.checkoutFormGroup.get('customer').value
     );
   }
+
+  handleMonthsAndYear() {
+    const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
+
+    const currentYear: number = new Date().getFullYear();
+    const selectedYear: number = Number(creditCardFormGroup.value.expirationYear);
+    
+    // if the current year your equals the selected year, then start with the current month
+
+    let startMonth: number;
+
+    if(currentYear === selectedYear){
+      startMonth = new Date().getMonth() + 1;
+    } else {
+      startMonth = 1;
+    }
+
+    this.luv2shopFormService.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        console.log("Retrieved credit card months: " +JSON.stringify(data));
+        this.creditCardMonths = data;
+      }
+    )
+  }
 }
