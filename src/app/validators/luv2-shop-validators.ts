79ad0aab7bcfee4c00c5244f648806392641ecc3
nguetcheu <dnguetcheu@gmail.com>
2023-05-23 18:28:42 +0200

    code du validator custom

--- src/app/validators/luv2-shop-validators.ts
+++ src/app/validators/luv2-shop-validators.ts
@@ -1,2 +1,13 @@
+import { FormControl, ValidationErrors } from '@angular/forms';
+
 export class Luv2ShopValidators {
+  static notOnlyWhiteSpace(control: FormControl): ValidationErrors {
+    if (control.value != null && control.value.trim().length === 0) {
+      // invalid, return error object
+      return { notOnlyWhitespace: true };
+    } else {
+      // valid return null
+      return null;
+    }
+  }
 }
