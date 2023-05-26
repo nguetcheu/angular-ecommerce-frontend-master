48845678df8e767285aa797370505d1424c41538
nguetcheu <dnguetcheu@gmail.com>
2023-05-26 17:35:15 +0200

    Création du fichier de configuration de connexion au service okta

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+  oidc: {
+    clientId: '0oa9pw31tzd7Fd3Ij5d7',
+    issuer: 'https://dev-79328914.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+  },
+};
