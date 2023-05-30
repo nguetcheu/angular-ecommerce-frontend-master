1476b9627a6185922649466f43e515ca54e59237
nguetcheu <dnguetcheu@gmail.com>
2023-05-30 19:32:42 +0200

    Fix bug css

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -14,6 +14,7 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
     localStorage.removeItem('email');
     localStorage.removeItem('token');
+    localStorage.removeItem('userEmail');
   }
 
   login() {
