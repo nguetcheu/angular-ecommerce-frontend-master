4a6c747993a31bcc31f2902ddf44ec2f6aafbc6c
nguetcheu <dnguetcheu@gmail.com>
2023-05-30 13:51:39 +0200

    Protection de la route menbers ok

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -3,22 +3,23 @@ import { AngularFireAuth } from '@angular/fire/auth';
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  Router,
   RouterStateSnapshot,
   UrlTree,
 } from '@angular/router';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  constructor(private afAuth: AngularFireAuth) {}
+  constructor(private afAuth: AngularFireAuth, public router: Router) {}
 
   async canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     const user = await this.afAuth.currentUser;
     const isAuthenticated = user ? true : false;
     if (!isAuthenticated) {
       alert('You must be menber user to acces this page');
+      this.router.navigate(['/login']);
     }
     return isAuthenticated;
   }
